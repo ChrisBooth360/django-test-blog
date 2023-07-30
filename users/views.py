9a48723e13fbc709e8e2a8222b13494abc07c931
Chris Booth <chris.booth.ct@gmail.com>
2023-07-30 19:36:38 +0200

    crispy form ui added

--- users/views.py
+++ users/views.py
@@ -5,8 +5,8 @@ from .forms import UserRegisterForm
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        form.save()
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
